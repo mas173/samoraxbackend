71111facba588f4e2f5f54cd532afac84d24312e
Vicky Raj <rajv60701@gmail.com>
2025-12-08 20:32:04 +0530

    frontend url added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(cookie_parser())
 app.use(cors({
-  origin:["http://localhost:5173"],
+  origin:["http://localhost:5173" ,"https://samosapoint.netlify.app/"],
   credentials:true
 }))
 
